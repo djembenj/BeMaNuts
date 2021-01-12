cf4b2db4a4fead324c98965be5146e99a0075115
Benjamin Jakubowski <benjamin.jakubowski@gmail.com>
2021-01-12 12:21:03 +0100

    Fichiers de LANG

---
+++ httpd-directory/.config_fr.php
@@ -0,0 +1,28 @@
+<?php
+// LANGUE FR //
+
+$titre_Index2 ="Mon Impact environnemental en Kg Co<sub>2</sub>eq";
+$titre_equipement ="Mon Equipement";
+$titre_mon_portable="Quel est mon ordinateur portable?";
+$date_mon_portable="Date";
+$titre_duration="Durée d'utilisation";
+$titre_screen="Quel est mon écran?";
+$titre_desktop="Quel est mon ordinateur de bureau?";
+$titre_server="Quel est mon serveur?";
+$titre_telephone="Quel est mon téléphone?";
+$titre_printer= "Quelle est mon imprimante?";
+$titre_audio="Quelle est mon enceinte connectée?";
+$titre_network="Quel est mon equipement réseau?";
+$titre_annuel="Annuel";
+$titre_tablette="Quelle est ma tablette?";
+$titre_an="an(s)";
+$select_equipement="Selectionner un équipement";
+$select_compagny="Selectionner une entreprise";
+$titre_moyenne_compagny="Moyenne Kg CO<sub>2</sub>eq par equipement";
+$type_equipement ="EQUIPEMENT";
+$titre_resultat="Votre résultat";
+$dw_resultat="Cliquer pour sauvegarder et partager";
+
+
+
+?>
---
+++ httpd-directory/.config_uk.php
@@ -0,0 +1,25 @@
+<?php
+// LANGUE UK //
+$titre_Index2 = "My environmental Impact in Kg Co<sub>2</sub>eq";
+$titre_equipement = "My Device";
+$titre_mon_portable= "What is my laptop?";
+$date_mon_portable="Date";
+$titre_duration= "Duration of use";
+$titre_screen= "Do I have an additional screen?";
+$titre_desktop= "What is my desktop?";
+$titre_server= "What is my server?";
+$titre_telephone= "What is my phone?";
+$titre_printer= "What is my printer?";
+$titre_audio="What is my smart speakers?";
+$titre_network="What is my network device?";
+$titre_tablette="Whate is my tablet?";
+$titre_annuel="Annual";
+$titre_an="year(s)";
+
+$select_equipement="Select a device";
+$select_compagny="Select a compagny";
+$titre_moyenne_compagny="Average Kg CO<sub>2</sub>eq per device";
+$type_equipement ="DEVICE";
+$titre_resultat="Your result";
+$dw_resultat="Click to download & share";
+?>
