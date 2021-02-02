e432b2108ad1d5a6ff6d299483d750abd9557e9a
djembenj <33101238+djembenj@users.noreply.github.com>
2021-02-02 15:56:42 +0100

    V1.1 WebResponsive

--- httpd-directory/.config_uk.php
+++ httpd-directory/.config_uk.php
@@ -1,5 +1,24 @@
 <?php
 // LANGUE UK //
+$nuts_title="<b>NUTS</b> Device Carbon Footprint Calculator!</b>";
+$nuts_title2="NUTS <strong>DIGITAL CO2eq CALCULATOR</strong>";
+$nuts_info_intro="Nuts by BeMa is a platform to shed light on <b>the environmental impacts</b> linked to our <b>use of digital technology.</b>";
+
+$nuts_info_introbis="In fact, we emit <b>Greenhouse Gases</b> (GHGs) with the use of our digital device.<br>";
+$nuts_info_introter="Nuts is a datalake gathering <b>manufacturer data around GHG emissions</b>,
+  for the different manufacturing phases of an equipment:<br>
+- <b>Manufacturing </b><br>
+- <b>Transport</b> <br>
+- <b>Use </b><br>
+- <b>End of life</b>";
+$ici="Let's Go";
+$help="Need help ? Watch this video";
+$envoyer="Send Message";
+
+$nuts_info_intro1="Nuts aims to make you aware of your GHG emissions (CO2eq) when using your equipment.<br>";
+$nuts_info_intro2="<br>However, if you are in a situation of not being able to prolong the use of your equipment, Nuts offers you to compensate by planting trees.";
+$nuts_info_intro3="A tip, just think about increasing the lifespan of your devices and you will already be a Digital Ecologist";
+$nuts_info_intro4="When using Nuts, clicking on \"More Info\" will give you access to all of the equipment's life cycle shows.";
 $titre_Index2 = "My environmental Impact in Kg Co<sub>2</sub>eq";
 $titre_equipement = "My Device";
 $titre_mon_portable= "What is my laptop?";
@@ -15,11 +34,33 @@ $titre_network="What is my network device?";
 $titre_tablette="Whate is my tablet?";
 $titre_annuel="Annual";
 $titre_an="year(s)";
+$titre_an1="year";
 
 $select_equipement="Select a device";
+$select_equipement_pc="Select a laptop";
+$select_equipement_monitor="Select a screen";
+$select_equipement_desktop="Select a desktop";
+$select_equipement_smartphone="Select a phone";
+$select_equipement_tablette="Select a tablet";
+$select_equipement_enceinte="Select an audio";
+
+
+
+
 $select_compagny="Select a compagny";
 $titre_moyenne_compagny="Average Kg CO<sub>2</sub>eq per device";
 $type_equipement ="DEVICE";
 $titre_resultat="Your result";
 $dw_resultat="Click to download & share";
+$detail="More details";
+
+$home1="OKAY, WHAT IS IT?";
+$home2="WHAT ?";
+$home3="About informations ?";
+$footer_question="Questions or Comments?";
+$footer_question2="GET IN TOUCH:";
+$footer_questions3="
+Do not hesitate to contact us for a remark or more, to participate in the Nuts adventure, and even a criticism.<br>
+<strong>Deviens Digital Ecologist</strong>";
+
 ?>
--- httpd-directory/index.php
+++ httpd-directory/index.php
@@ -1,9 +1,134 @@
-<?php session_destroy(); session_regenerate_id(true); ?>
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-<title>Nuts by BeMa Digital Ecologist</title>
-<link rel="shortcut icon" type="image/x-icon" href="images/nuts.ico">
-</head>
-<FRAMESET rows="100%,*" border=0 frameborder=0 framespacing=0>
-<FRAME SRC="home_public.php" MARGINWIDTH=0 MARGINHEIGHT=0 SCROLLING=auto noresize>
+<?php
+
+include("include/connexion.php");
+include("include/auth.php");
+include("include/equipement.php");
+include("include/menu.php");
+
+if ( $_GET['LANG'] ) {
+  $LANG=$_GET['LANG'];
+}
+else {
+  $_SESSION['Lang']="fr";
+  $LANG="fr";
+}
+include(".config_$LANG.php");
+session_start();
+//verif_session();
+entete($LANG);
+?>
+	<body class="homepage is-preload">
+		<div id="page-wrapper">
+			<!-- Header -->
+				<section id="header">
+					<div class="container">
+							<?php menu();?>
+							<h2><?php echo $nuts_title;?></h2>
+						<div class="row aln-center">
+							<div class="col-4 col-6-medium col-12-small">
+									<section>
+										<a href="calculator.php" class="image featured"><img src="images/info/calculator.png" alt="" /></a>
+										<header>
+											<h3><?php echo $home1;?></h3>
+										</header>
+										<p><?php echo $nuts_info_intro;?></p>
+									</section>
+							</div>
+							<div class="col-4 col-6-medium col-12-small">
+									<section>
+										<a href="calculator.php#" class="image featured"><img src="images/info/nutsinfomiddel.png" alt="" /></a>
+										<header>
+											<h3><?php echo $home2;?></h3>
+										</header>
+										<p><?php echo $nuts_info_introbis;?></p>
+									</section>
+							</div>
+							<div id="content" class="col-4 col-6-medium col-12-small">
+									<section>
+										<a href="calculator.php#" class="image featured"><img src="images/menu/datalake.png" alt="" /></a>
+										<header>
+											<h3><?php echo $home3;?></h3>
+										</header>
+											<p><?php  echo $nuts_info_introter;?></p>
+									</section>
+							</div>
+							<div class="col-12">
+								<ul class="actions">
+									<li><a href="calculator.php" class="button icon solid fa-file"><?php echo $ici;?></a></li>
+								</ul>
+							</div>
+						</div>
+					</div>
+				</section>
+			<!-- Banner -->
+				<section id="banner">
+					<div class="container">
+						<p><?php echo $nuts_info_intro1;?></p>
+					</div>
+				</section>
+			<!-- Main -->
+				<section id="main">
+					<div class="container">
+						<div class="row">
+							<!-- Content -->
+								<div id="content" class="col-8 col-12-medium">
+									<!-- Post -->
+										<article class="box post">
+											<h3><?php echo $nuts_info_intro3;?></h3>
+											<p><?php echo $nuts_info_intro2;?></p>
+											<a href="#"><img src="images/menu/arbre.png" alt="" /></a>
+											<p><?php echo $nuts_info_intro4;?></p>
+											<img src="images/info/full-informations.png" alt=""  />
+											<ul class="actions">
+												<li><a href="calculator.php" class="button icon solid fa-file"><?php echo $ici;?></a></li>
+											</ul>
+												<p><?php echo $help;?></p>
+										<iframe width="350" height="" src="https://www.youtube.com/embed/3BSpE5FzIBE" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></td>
+										</article>
+								</div>
+							<!-- Sidebar -->
+								<div id="sidebar" class="col-4 col-12-medium">
+								<!-- Excerpts -->
+										<section>
+											<ul class="divided">
+												<li>
+												<!-- Excerpt -->
+														<article class="box excerpt">
+															<header>
+																<span class="date">Février 01</span>
+																<h3><a href="#">Nouvelle version</a></h3>
+															</header>
+															<p>Vous avez été nombreux à utiliser la Calculatrice dès son lancement, et donc voici une version Web Responsive, afin de vous apporter plus de confort</p>
+														</article>
+
+												</li>
+												<li>
+												<!-- Excerpt -->
+														<article class="box excerpt">
+															<header>
+																<span class="date">Janvier 28</span>
+																<h3><a href="#">Wahooo</a></h3>
+															</header>
+															<p>Merci Merci, vous avez été 707 à utiliser Nuts pour sa première journée.</p>
+														</article>
+
+												</li>
+												<li>
+													<!-- Excerpt -->
+														<article class="box excerpt">
+															<header>
+																<span class="date">Janvier 27</span>
+																<h3><a href="#">Let's Go</a></h3>
+															</header>
+															<p>Après plusieurs mois de documentations, de prises de contacts avec les constructeur Nuts, sort la version 1.0 de sa calculatrice grand public.</p>
+														</article>
+
+												</li>
+											</ul>
+										</section>
+								</div>
+						</div>
+					</div>
+				</section>
+
+			<?php footer();?>
